5f42fcf1178cf1aa19754f5e28d45b1a21df1ba6
yangglemu <yangglemu@qq.com>
2018-06-18 15:49:30 +0800

    yb

--- ClientSender/DateSelect.Designer.cs
+++ ClientSender/DateSelect.Designer.cs
@@ -31,41 +31,31 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DateSelect));
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.button_OK = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(36, 18);
+            this.monthCalendar1.Location = new System.Drawing.Point(22, 19);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.ShowTodayCircle = false;
             this.monthCalendar1.TabIndex = 0;
             // 
             // button_OK
             // 
-            this.button_OK.Location = new System.Drawing.Point(105, 219);
+            this.button_OK.Location = new System.Drawing.Point(95, 220);
             this.button_OK.Name = "button_OK";
             this.button_OK.Size = new System.Drawing.Size(75, 23);
             this.button_OK.TabIndex = 1;
             this.button_OK.Text = "确定";
-            this.button_OK.UseVisualStyleBackColor = true;            
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Location = new System.Drawing.Point(17, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(266, 123);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
+            this.button_OK.UseVisualStyleBackColor = true;
             // 
             // DateSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(264, 262);
             this.Controls.Add(this.button_OK);
             this.Controls.Add(this.monthCalendar1);
-            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -82,6 +72,5 @@
 
         private System.Windows.Forms.MonthCalendar monthCalendar1;
         private System.Windows.Forms.Button button_OK;
-        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
